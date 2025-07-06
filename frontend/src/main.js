2b48e2e64d060eef4f632ea268e939da238c0229
Hanjaya27 <hanjayahartono27@gmail.com>
2025-07-07 00:44:27 +0700

    Update

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Impor router
+import animateOnScroll from './directives/animateOnScroll';
+
+const app = createApp(App);
+app.directive('animate-on-scroll', animateOnScroll);
+app.use(router); // Gunakan router
+app.mount('#app');
